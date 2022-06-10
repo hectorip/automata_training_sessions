89b18a2bd31b1f34299dcd3e1d5896c39ab1c9d3
Fernando Niño <ninofnj@gmail.com>
2022-06-10 16:44:03 -0500

    tests kata-library: Complete tests

--- kata-library/lib/library_test.rb
+++ kata-library/lib/library_test.rb
@@ -3,14 +3,13 @@ require "test/unit"
 
 class TestLibrary < Test::Unit::TestCase
 
-=begin
   def setup
     @library = Library.new
+    @library.load
   end
-=end
 
   def test_load_books
-    library = Library.new(["books.csv"])
+    library = Library.new(["data/books.csv"])
     # Load items
     library.load
     # Assert count loaded items
@@ -44,9 +43,9 @@ class TestLibrary < Test::Unit::TestCase
     library = Library.new(["authors.csv"])
     # Load items
     library.load
-    # Assert count loaded items
 
-    assert_equal(6, library.papers.count)
+    # Assert count loaded items
+    assert_equal(6, library.authors.count)
 
     # Assert some info of first author
     first_author = library.papers[0]
@@ -54,5 +53,36 @@ class TestLibrary < Test::Unit::TestCase
     # Assert some info of last author
     last_author = library.papers[-1]
     assert_equal("null-rabe@echocat.org", last_author.email)
+  end
+
+  def test_load_all_papers
+    # Assert count loaded items (books and magazines)
+    assert_equal(8 + 6, @library.papers.count)
+  end
+
+  def test_find_by_isbn
+    paper = @library.find("1024-5245-8584")[0]
+    assert_not_nil(paper)
+    assert_equal("1024-5245-8584", paper.isbn)
+    assert_equal("Genial italienisch", paper.title)
+  end
+
+  def test_find_by_author
+    papers = @library.find(authors="null-walter@echocat.org")
+    # 4
+    assert_equal(4, papers.count)
+  end
+
+  def test_pretty_print_book
+    paper = @library.find("1024-5245-8584")
+    long_string = <<EOS
+title: O'Reillys Kochbuch für Geeks
+isbn: 2215-0012-5487
+authors: null-mueller@echocat.org
+description: Nach landläufiger Meinung leben Geeks von Cola und TK-Pizza, die sie nachts am Rechner geistesabwesend in sich reinschlingen. Soweit das Klischee! Dass aber Kochen viel mit Programmieren zu tun hat, dass es nämlich ähnlich kreativ ist, dass viele Wege zum individuellen Ziel führen und dass manche Rezepte einfach nur abgefahren und -- ja, geekig sind: Das zeigen zwei Köchinnen in diesem Buch.
+EOS
+
+    assert_not_nil(paper)
+    assert_equal(long_string, paper.full_print)
   end
 end
