5b5427481953648e59dcea486a956f20a32c046f
Fernando Niño <ninofnj@gmail.com>
2022-07-01 11:46:14 -0500

    feature: load books

--- kata-library/lib/library.rb
+++ kata-library/lib/library.rb
@@ -9,22 +9,45 @@ class Author
   end
 end
 
+class Paper
+  attr_reader :title, :isbn, :authors, :description, :published_at
+  #title;isbn;authors;description;publishedAt
+  def initialize(title, isbn, authors, description="", published_at="")
+    @title = title
+    @isbn = isbn
+    @authors = authors
+    @description = description
+    @published_at = published_at
+  end
+end
+
 class Library
-  attr_reader :authors
+  attr_reader :authors, :papers
 
   def initialize
     @authors = []
+    @papers = []
   end
 
-  def load_authors(files, col_sep = ';')
-    puts 'hola'
+  def load_csv(files, col_sep = ";")
     files.each do |file|
       path_file = File.join(File.dirname(__FILE__), file)
       csv_text = File.read(path_file)
-      csv = CSV.parse(csv_text, :headers => true, :col_sep => col_sep)
-      csv.each do |row|
-        @authors.append(Author.new(row[0], row[1], row[2]))
-      end
+      return CSV.parse(csv_text, :headers => true, :col_sep => col_sep)
+    end
+  end
+
+  def load_authors(files, col_sep = ';')
+    csv = load_csv(files, col_sep)
+    csv.each do |row|
+      @authors.append(Author.new(row[0], row[1], row[2]))
+    end
+  end
+
+  def load_papers(files, col_sep = ';')
+    csv = load_csv(files, col_sep)
+    csv.each do |row|
+      @papers.append(Paper.new(row[0], row[1], row[2], row[3]))
     end
   end
 end
--- kata-library/lib/library_test.rb
+++ kata-library/lib/library_test.rb
@@ -6,19 +6,25 @@ class TestLibrary < Test::Unit::TestCase
 
 
   def test_load_authors
-    library = Library.new()
+    library = Library.new
     library.load_authors(["../data/authors.csv"])
 
     assert_equal(6, library.authors.count)
 
     assert_equal("null-walter@echocat.org", library.authors[0].email)
-
     assert_equal("Harald", library.authors[-1].first_name)
 
   end
 
-  # def test_load_books
-  #
-  # end
+
+  def test_load_books
+    library = Library.new
+    library.load_papers(["../data/books.csv"])
+
+    assert_equal(8, library.papers.count)
+
+    assert_equal("5554-5545-4518", library.papers[0].isbn)
+    assert_equal("Schuhbecks Kochschule. Kochen lernen mit Alfons Schuhbeck ", library.papers[-1].title)
+  end
 
 end
